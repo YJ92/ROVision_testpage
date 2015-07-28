afb8291dcc109bc144f36631b455423adc9a3c34
root <root@beaglebone.localdomain>
2015-07-28 06:56:11 +0000

    modify

--- app.js
+++ app.js
@@ -76,7 +76,7 @@ io.on('connection',function(socket){
   socket.on("serial_connect",function(data){
     switch(data){
       case 'connect' :
-        serialPort = new SerialPort("COM9",{baudrate : 9600},true,function(err){
+        serialPort = new SerialPort("/dev/ttyO1",{baudrate : 9600},true,function(err){
           if(err){
             console.log("connect error");
             socket.emit("serial_connect","error");
