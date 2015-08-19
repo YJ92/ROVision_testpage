fb163494de941b5bbe1f32b73d12ca7a7fad2aa9
YJ92 <naga92@naver.com>
2015-08-20 01:07:43 +0900

    commit

--- app.js
+++ app.js
@@ -12,10 +12,7 @@ var app = express();
 
 var cleanData = '';
 var readData = '';
-// Seirlaport
-var sp = require('serialport');
-var UART1_port = "/dev/ttyO1";
-var option = { baudrate : 9600 };
+var SerialPort = require('serialport').SerialPort;
 var serialPort = '';
 
 // view engine setup
@@ -79,7 +76,7 @@ io.on('connection',function(socket){
   socket.on("serial_connect",function(data){
     switch(data){
       case 'connect' :
-        serialPort = new sp.SerialPort(UART1_port, option,true,function(err){
+        serialPort = new SerialPort("COM9",{baudrate : 9600},true,function(err){
           if(err){
             console.log("connect error");
             socket.emit("serial_connect","error");
